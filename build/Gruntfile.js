45b7f396b858bd856454dd50af0711ba19d37e80
techninja <james@tn42.com>
2015-07-27 17:08:55 -0700

    Initial commit of Electron wrapper & build setup for #2

---
+++ build/Gruntfile.js
@@ -0,0 +1,74 @@
+var path = require('path');
+
+module.exports = function(grunt) {
+  var electronVer = '0.30.1'; // Electron build version
+
+  // Load the plugins...
+  grunt.loadNpmTasks('grunt-electron');
+  if (process.platform === 'win32') grunt.loadNpmTasks('grunt-electron-installer');
+  if (process.platform === 'darwin') grunt.loadNpmTasks('grunt-appdmg');
+
+  // Load the tasks in '/tasks'
+  grunt.loadTasks('tasks');
+
+  // Set all subsequent paths to the relative to the root of the repo
+  grunt.file.setBase(path.resolve('..'));
+
+  var appInfo = grunt.file.readJSON('package.json');
+  var version = appInfo.version;
+  var buildIgnore = "build";
+
+  // Project configuration.
+  grunt.initConfig({
+    name: appInfo.name,
+    pkg: appInfo,
+    electron: {
+      macbuild: {
+        options: {
+          name: appInfo.name,
+          dir: './',
+          out: 'build/dist',
+          icon: 'resources/mac/app.icns',
+          version: electronVer,
+          platform: 'darwin',
+          arch: 'x64',
+          ignore: buildIgnore,
+          'app-version': version,
+          overwrite: true,
+          prune: true,
+          'app-bundle-id': 'pancakecreator-main',
+          'helper-bundle-id': 'pancakecreator-helper'
+        }
+      },
+      winbuild: {
+        options: {
+          name: appInfo.name,
+          dir: './',
+          out: 'build/dist',
+          icon: 'resources/win32/app.ico',
+          version: electronVer,
+          platform: 'win32',
+          arch: 'x64,ia32',
+          ignore: buildIgnore,
+          'app-version': version,
+          overwrite: true,
+          prune: true,
+          'version-string': {
+            CompanyName: 'PancakeBot Inc.',
+            LegalCopyright: 'Copyright (C) PancakeBot Inc., all rights reserved. Code under Apache v2.0 free and open source license.',
+            FileDescription: appInfo.name,
+            OriginalFilename: appInfo.name + '.exe',
+            FileVersion: electronVer,
+            ProductVersion: version,
+            ProductName: appInfo.name,
+            InternalName: appInfo.name
+          }
+        }
+      }
+    }
+  });
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['pre-build']);
+};
---
+++ build/tasks/build-task.js
@@ -0,0 +1,63 @@
+/*
+ * @file Build task wrapper and cleanup code.
+ * (electron-packager does most of the work)
+ */
+
+module.exports = function(grunt) {
+  var log = grunt.log.writeln;
+  var conf = grunt.config;
+  var fs = require('./task-helpers')(grunt);
+  var fsp = require('fs-plus');
+  var path = require('path');
+
+  grunt.registerTask('build-win', 'Build the release application for windows.', function(){
+    log('Running electon-packager for win build...');
+    grunt.task.run('electron:winbuild', 'build-win-icon');
+
+    // If we're on Win32, go ahead and run create-windows-installer
+    if (process.platform === 'win32') {
+      if (fsp.existsSync(conf('create-windows-installer.outputDirectory'))) {
+        fs.rm(conf('create-windows-installer.outputDirectory'));
+      }
+
+      grunt.task.run('build-win-install');
+    }
+  });
+
+  grunt.registerTask('build-win-icon', 'Change out the icon on the built windows exe.', function(){
+    log('Changing windows executable icon...');
+
+    var done = this.async();
+    var shellExePath = path.join('build', 'dist', conf('name') + '-win32-x64', conf('name') + '.exe');
+    var iconPath = path.resolve('resources', 'win', 'app.ico');
+    var rcedit = require('rcedit');
+
+    return rcedit(shellExePath, {icon: iconPath}, done);
+  });
+
+  grunt.registerTask('build-win-install', 'Create Windows Installer.', function(){
+    grunt.task.run('create-windows-installer:64', 'create-windows-installer:32', 'build-win-install-post');
+  });
+
+  grunt.registerTask('build-win-install-post', 'Create Windows installer post cleanup.', function(){
+    log('Cleanup windows install...');
+    ['32', '64'].map(function(arch) {
+      var p = conf('create-windows-installer.' + arch + '.outputDirectory');
+      fs.mv(p + 'Setup.exe', 'build/dist/Install_' + conf('name') + '_Win_' + arch + 'bit_v' + conf('pkg.version') + '.exe');
+      fs.rm(p);
+    });
+  });
+
+  grunt.registerTask('build-mac', 'Build the release application for OS X.', function(){
+    grunt.task.run('electron:macbuild');
+
+    // If we're on Mac, go ahead and run appdmg
+    if (process.platform === 'darwin') {
+      if (fsp.existsSync(conf('appdmg.target.dest'))) {
+        fs.rm(conf('appdmg.target.dest'));
+      }
+
+      grunt.task.run('appdmg');
+    }
+  });
+};
