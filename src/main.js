35c565afa6e081d17c1a6ea40a425daf7623f295
techninja <james@tn42.com>
2015-08-05 16:06:40 -0700

    Quit app with window close on mac, for #17

--- src/main.js
+++ src/main.js
@@ -98,13 +98,9 @@ function settingsInit() {
  * Initialize the windows/attach menus
  */
 function windowInit() {
-  // Quit when all windows are closed.
+  // Quit when all windows are closed (including OSX).
   app.on('window-all-closed', function() {
-    // On OSX it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
       app.quit();
-    }
   });
 
   // This method will be called when Electron has done all the initialization
