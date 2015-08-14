3b54a836e23e77bc045d18554649ef355b00f9c1
techninja <james@tn42.com>
2015-08-14 00:12:19 -0700

    Finally remove silly blob creation

--- src/editor.ps.js
+++ src/editor.ps.js
@@ -24,36 +24,8 @@ var values = {
   maxRadius: 90
 };
 
-createPaths();
-
 var toolPen = require('./tools/tool.pen')(paper);
 var toolSelect = require('./tools/tool.select')(paper);
-
-function createPaths() {
-  var radiusDelta = values.maxRadius - values.minRadius;
-  var pointsDelta = values.maxPoints - values.minPoints;
-  for (var i = 0; i < values.paths; i++) {
-    var radius = values.minRadius + Math.random() * radiusDelta;
-    var points = values.minPoints + Math.floor(Math.random() * pointsDelta);
-    var path = createBlob(view.size * Point.random(), radius, points);
-    path.strokeColor = paper.pancakeShades[0];
-    path.strokeWidth = paper.strokeWidth;
-  };
-}
-
-function createBlob(center, maxRadius, points) {
-  var path = new Path();
-  path.closed = true;
-  for (var i = 0; i < points; i++) {
-    var delta = new Point({
-      length: (maxRadius * 0.5) + (Math.random() * maxRadius * 0.5),
-      angle: (360 / points) * i
-    });
-    path.add(center + delta);
-  }
-  path.smooth();
-  return path;
-}
 
 var $editor = $('#editor');
 paper.setCursor = function(type) {
