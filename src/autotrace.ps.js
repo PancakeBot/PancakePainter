fbed06470d5b495e0f7afe4deef82857a23f8ccc
techninja <james@tn42.com>
2016-12-30 01:46:14 -0800

    Massive tracing refactor, move to promise based workflow

--- src/autotrace.ps.js
+++ src/autotrace.ps.js
@@ -7,6 +7,7 @@
    _, paper, Layer, Group, Raster, view, project, autoTraceLoadedInit, app,
    Shape, Point, mainWindow
  */
+ var jimp = require('jimp');
 
 // Localize autotrace to be the parent window object (holds settings, paths).
 var autotrace = mainWindow.overlay.windows.autotrace;
@@ -35,16 +36,12 @@ _.each(['utils', 'autotrace'], function(helperName) {
 // Initialize the colors to snap to based on the pancake shades.
 paper.utils.snapColorSetup(app.constants.pancakeShades);
 
-// ============================================================================
-// How should this work:
-// 1. Initially load the image on window load (this saves the bmp to be traced)
-// 2. Have a reload function (called immediately) that re-runs the traces
-// required
-// 3. Trigger each on update
-
-var jimp = require('jimp');
-
-// Initially load the trace image
+/**
+ * Initially load and size the input trace image, rasterizing and outputting
+ * an intermediary file at a fixed size.
+ * @return {Promise}
+ *   When promise is resolved, autotrace.tracebmp is saved & ready.
+ */
 paper.loadTraceImage = function() {
   imageLayer.removeChildren();
   imageLayer.activate();
@@ -84,7 +81,12 @@ paper.loadTraceImage = function() {
   });
 };
 
-// Render the trace image through jimp for vector tracing.
+/**
+ * Render the trace raster intermediary image through jimp for extra processing
+ * to create the final raster for tracing.
+ * @return {Promise}
+ *   When promise is resolved, autotrace.tracebmp is saved & ready.
+ */
 paper.renderTraceImage = function() {
   return new Promise(function(resolve, reject) {
     try {
@@ -115,68 +117,34 @@ paper.renderTraceImage = function() {
   });
 };
 
-// Render and display the trace.
+/**
+ * Render and display trace..
+ * @return {Promise}
+ *   When promise is resolved, tracing is complete.
+ */
 paper.renderTraceVector = function() {
   tempLayer.activate();
   tempLayer.removeChildren();
 
   switch (autotrace.settings.tracetype) {
     case 'mixed':
-      paper.renderMixedVector();
-      break;
+      return renderMixedVector();
     case 'fills':
-      paper.renderFillsVector();
-      break;
+      return renderFillsVector();
     case 'lines':
-      paper.renderLinesVector();
-      break;
+      return renderLinesVector();
   }
 };
 
 
-paper.renderMixedVector = function() {
-  var lines, fills, mask;
-  var options = {
-    backgroundColor: autotrace.settings.transparent.substr(1),
-    colorCount: autotrace.settings.posterize,
-  };
+//==============================================================================
 
-  var img = autotrace.tracebmp;
-  paper.autotrace.getImageLines(img, options).then(function(data) {
-    lines = tempLayer.importSVG(data);
-    autotrace.paper.activate();
-    paper.tracedGroup = new Group([lines]);
-
-    lines.strokeWidth = 5;
-    return paper.autotrace.getImageFills(img, options);
-  }).then(function(data) {
-    var offset = 6;
-    fills = tempLayer.importSVG(data);
-
-    mask = fills.clone();
-
-    // Negative offset fills to destroy thin shapes.
-    mask = paper.utils.offsetPath(mask.children[0], -offset, 5);
-
-    // Only if there's a result from the destructive process above...
-    if (mask) {
-      // Then unoffset the same fill to use as a mask.
-      mask = paper.utils.offsetPath(mask, offset, 5);
-      paper.tracedGroup.addChild(mask);
-    }
-
-    paper.tracedGroup.position.y = view.center.y;
-    paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
-
-    // Remove any previous work and append built data to svgLayer.
-    svgLayer.removeChildren();
-    svgLayer.addChild(paper.tracedGroup);
-
-    paper.normalizeSVG();
-  });
-};
-
-paper.renderFillsVector = function() {
+/**
+ * Render a fill only vector trace from the source bitmap.
+ * @return {Promise}
+ *   When promise is resolved, tracing is complete.
+ */
+function renderFillsVector() {
   var fills;
   var options = {
     backgroundColor: autotrace.settings.transparent.substr(1),
@@ -184,23 +152,38 @@ paper.renderFillsVector = function() {
   };
 
   var img = autotrace.tracebmp;
-  paper.autotrace.getImageFills(img, options).then(function(data) {
-    fills = tempLayer.importSVG(data);
-    autotrace.paper.activate();
-    paper.tracedGroup = fills;
+  return new Promise(function(resolve, reject) {
+    paper.autotrace.getImageFills(img, options).then(function(data) {
+      fills = tempLayer.importSVG(data);
+      if (fills) {
+        autotrace.paper.activate();
+        paper.tracedGroup = fills;
 
-    paper.tracedGroup.position.y = view.center.y;
-    paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
+        paper.tracedGroup.position.y = view.center.y;
+        paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
 
-    // Remove any previous work and append built data to svgLayer.
-    svgLayer.removeChildren();
-    svgLayer.addChild(paper.tracedGroup);
+        // Remove any previous work and append built data to svgLayer.
+        svgLayer.removeChildren();
+        svgLayer.addChild(paper.tracedGroup);
 
-    paper.normalizeSVG();
+        // Normalize & recolor the final trace.
+        normalizeSVG();
+
+        // Resolve the promise.
+        resolve();
+      } else {
+        reject(Error('No data returned from trace.'));
+      }
+    });
   });
-};
+}
 
-paper.renderLinesVector = function() {
+/**
+ * Render a centerline only vector trace from the source bitmap.
+ * @return {Promise}
+ *   When promise is resolved, tracing is complete.
+ */
+function renderLinesVector() {
   var lines;
   var options = {
     backgroundColor: autotrace.settings.transparent.substr(1),
@@ -208,22 +191,89 @@ paper.renderLinesVector = function() {
   };
 
   var img = autotrace.tracebmp;
-  paper.autotrace.getImageLines(img, options).then(function(data) {
-    lines = tempLayer.importSVG(data);
-    lines.strokeWidth = 5;
-    autotrace.paper.activate();
-    paper.tracedGroup = lines;
+  return new Promise(function(resolve, reject) {
+    paper.autotrace.getImageLines(img, options).then(function(data) {
+      lines = tempLayer.importSVG(data);
+      if (lines) {
+        lines.strokeWidth = 5;
+        autotrace.paper.activate();
+        paper.tracedGroup = lines;
 
-    paper.tracedGroup.position.y = view.center.y;
-    paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
+        paper.tracedGroup.position.y = view.center.y;
+        paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
 
-    // Remove any previous work and append built data to svgLayer.
-    svgLayer.removeChildren();
-    svgLayer.addChild(paper.tracedGroup);
+        // Remove any previous work and append built data to svgLayer.
+        svgLayer.removeChildren();
+        svgLayer.addChild(paper.tracedGroup);
 
-    paper.normalizeSVG();
+        // Normalize & recolor the final trace.
+        normalizeSVG();
+
+        // Resolve the promise.
+        resolve();
+      } else {
+        reject(Error('No data returned from trace.'));
+      }
+    });
   });
-};
+}
+
+/**
+ * Render a mixed line/fill vector trace from the source bitmap.
+ * @return {Promise}
+ *   When promise is resolved, tracing is complete.
+ */
+function renderMixedVector() {
+  var lines, fills, mask;
+  var options = {
+    backgroundColor: autotrace.settings.transparent.substr(1),
+    colorCount: autotrace.settings.posterize,
+  };
+
+  var img = autotrace.tracebmp;
+  return new Promise(function(resolve, reject) {
+    paper.autotrace.getImageLines(img, options).then(function(data) {
+      lines = tempLayer.importSVG(data);
+      autotrace.paper.activate();
+      paper.tracedGroup = new Group([lines]);
+
+      lines.strokeWidth = 5;
+      return paper.autotrace.getImageFills(img, options);
+    }).then(function(data) {
+      var offset = 6;
+      fills = tempLayer.importSVG(data);
+
+      // Reject the promise at this point as we have no lines or fills.
+      if (!fills && !lines) {
+        reject(Error('No data returned from trace.'));
+      }
+
+      mask = fills.clone();
+
+      // Negative offset fills to destroy thin shapes.
+      mask = paper.utils.offsetPath(mask.children[0], -offset, 5);
+
+      // Only if there's a result from the destructive process above...
+      if (mask) {
+        // Then unoffset the same fill to use as a mask.
+        mask = paper.utils.offsetPath(mask, offset, 5);
+        paper.tracedGroup.addChild(mask);
+      }
+
+      paper.tracedGroup.position.y = view.center.y;
+      paper.tracedGroup.position.x = (view.bounds.width / 4) * 3;
+
+      // Remove any previous work and append built data to svgLayer.
+      svgLayer.removeChildren();
+      svgLayer.addChild(paper.tracedGroup);
+
+      normalizeSVG();
+
+      // Resolve the promise.
+      resolve();
+    });
+  });
+}
 
 paper.normalizeSVG = function() {
   autotrace.paper.activate();
